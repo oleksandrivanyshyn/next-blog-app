c2fd88ed96e902aab5fdcd1130f5321931e632f0
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-30 22:26:10 +0300

    feat: add PostsGrid component and integrate it into FeaturedPosts section

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import PostsGrid from '../posts/posts-grid';
+import classes from './featured-posts.module.css';
 
-const FeaturedPosts = () => {
-  return <div></div>;
-};
+function FeaturedPosts(props) {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts} />
+    </section>
+  );
+}
 
 export default FeaturedPosts;
