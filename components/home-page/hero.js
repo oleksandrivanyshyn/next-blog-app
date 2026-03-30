d38239e6d18ca3d9d8599d087d354f7c4c47910d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-30 22:15:03 +0300

    feat: implement Hero component on the home page with placeholder image

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,7 +1,20 @@
-import React from 'react';
-
+import classes from './hero.module.css';
+import Image from 'next/image';
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I`m Max</h1>
+      <p>I blog about web development</p>
+    </section>
+  );
 };
 
 export default Hero;
