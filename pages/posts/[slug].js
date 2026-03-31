c9b9352e192c60d1ac8192bc064bbe806b32edce
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-31 15:37:42 +0300

    feat: add PostDetailPage with dynamic rendering and content styling

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,39 @@
-const PostDetailPage = () => {
-  return <div></div>;
-};
+import { Fragment } from 'react';
+
+import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
+
+function PostDetailPage(props) {
+  return (
+    <Fragment>
+      <PostContent post={props.post} />
+    </Fragment>
+  );
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
 
 export default PostDetailPage;
